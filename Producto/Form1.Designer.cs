aa0ee588aaab5917c26dc8aec67fbed9db2cedc4
dara0212 <90067132+dara0212@users.noreply.github.com>
2021-09-04 10:25:44 -0600

    Agregar archivos de proyecto.

---
+++ Producto/Form1.Designer.cs
@@ -0,0 +1,336 @@
+﻿
+namespace Producto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbldatos = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.cmbmedida = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtcaducidad = new System.Windows.Forms.TextBox();
+            this.txtprecio = new System.Windows.Forms.TextBox();
+            this.txtcantidad = new System.Windows.Forms.TextBox();
+            this.Txtdescripcion = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btnagregar = new System.Windows.Forms.Button();
+            this.btneliminar = new System.Windows.Forms.Button();
+            this.btnactualizar = new System.Windows.Forms.Button();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Producto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Descripcion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Precio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Cantidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Caducidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cmbelegir = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.tbldatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbldatos
+            // 
+            this.tbldatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tbldatos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.Producto,
+            this.Descripcion,
+            this.Precio,
+            this.Cantidad,
+            this.Caducidad});
+            this.tbldatos.Location = new System.Drawing.Point(-44, 316);
+            this.tbldatos.Name = "tbldatos";
+            this.tbldatos.Size = new System.Drawing.Size(723, 150);
+            this.tbldatos.TabIndex = 0;
+            this.tbldatos.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.tbldatos_CellClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Id";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(175, 52);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 20);
+            this.txtId.TabIndex = 2;
+            // 
+            // cmbmedida
+            // 
+            this.cmbmedida.FormattingEnabled = true;
+            this.cmbmedida.Location = new System.Drawing.Point(175, 274);
+            this.cmbmedida.Name = "cmbmedida";
+            this.cmbmedida.Size = new System.Drawing.Size(121, 21);
+            this.cmbmedida.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Nombre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Descripcion";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 182);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Cantidad";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(34, 213);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Precio";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(34, 238);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Caducidad";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(34, 277);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(93, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Unidad de medida";
+            // 
+            // txtcaducidad
+            // 
+            this.txtcaducidad.Location = new System.Drawing.Point(175, 241);
+            this.txtcaducidad.Name = "txtcaducidad";
+            this.txtcaducidad.Size = new System.Drawing.Size(100, 20);
+            this.txtcaducidad.TabIndex = 11;
+            // 
+            // txtprecio
+            // 
+            this.txtprecio.Location = new System.Drawing.Point(175, 215);
+            this.txtprecio.Name = "txtprecio";
+            this.txtprecio.Size = new System.Drawing.Size(100, 20);
+            this.txtprecio.TabIndex = 12;
+            // 
+            // txtcantidad
+            // 
+            this.txtcantidad.Location = new System.Drawing.Point(175, 182);
+            this.txtcantidad.Name = "txtcantidad";
+            this.txtcantidad.Size = new System.Drawing.Size(100, 20);
+            this.txtcantidad.TabIndex = 13;
+            // 
+            // Txtdescripcion
+            // 
+            this.Txtdescripcion.Location = new System.Drawing.Point(175, 143);
+            this.Txtdescripcion.Name = "Txtdescripcion";
+            this.Txtdescripcion.Size = new System.Drawing.Size(100, 20);
+            this.Txtdescripcion.TabIndex = 14;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(175, 89);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 15;
+            // 
+            // btnagregar
+            // 
+            this.btnagregar.Location = new System.Drawing.Point(644, 89);
+            this.btnagregar.Name = "btnagregar";
+            this.btnagregar.Size = new System.Drawing.Size(75, 23);
+            this.btnagregar.TabIndex = 16;
+            this.btnagregar.Text = "Agregar";
+            this.btnagregar.UseVisualStyleBackColor = true;
+            this.btnagregar.Click += new System.EventHandler(this.btnagregar_Click);
+            // 
+            // btneliminar
+            // 
+            this.btneliminar.Location = new System.Drawing.Point(635, 143);
+            this.btneliminar.Name = "btneliminar";
+            this.btneliminar.Size = new System.Drawing.Size(75, 23);
+            this.btneliminar.TabIndex = 17;
+            this.btneliminar.Text = "Eliminar";
+            this.btneliminar.UseVisualStyleBackColor = true;
+            this.btneliminar.Click += new System.EventHandler(this.btneliminar_Click);
+            // 
+            // btnactualizar
+            // 
+            this.btnactualizar.Location = new System.Drawing.Point(635, 203);
+            this.btnactualizar.Name = "btnactualizar";
+            this.btnactualizar.Size = new System.Drawing.Size(75, 23);
+            this.btnactualizar.TabIndex = 18;
+            this.btnactualizar.Text = "Actualizar";
+            this.btnactualizar.UseVisualStyleBackColor = true;
+            this.btnactualizar.Click += new System.EventHandler(this.btnactualizar_Click);
+            // 
+            // Id
+            // 
+            this.Id.HeaderText = "Id";
+            this.Id.Name = "Id";
+            // 
+            // Producto
+            // 
+            this.Producto.HeaderText = "Producto";
+            this.Producto.Name = "Producto";
+            // 
+            // Descripcion
+            // 
+            this.Descripcion.HeaderText = "Descripcion";
+            this.Descripcion.Name = "Descripcion";
+            // 
+            // Precio
+            // 
+            this.Precio.HeaderText = "Precio";
+            this.Precio.Name = "Precio";
+            // 
+            // Cantidad
+            // 
+            this.Cantidad.HeaderText = "Cantidad";
+            this.Cantidad.Name = "Cantidad";
+            // 
+            // Caducidad
+            // 
+            this.Caducidad.HeaderText = "Caducidad";
+            this.Caducidad.Name = "Caducidad";
+            // 
+            // cmbelegir
+            // 
+            this.cmbelegir.FormattingEnabled = true;
+            this.cmbelegir.Items.AddRange(new object[] {
+            "Unidades de medida",
+            "Fecha de caducidad",
+            "Id",
+            "Precio"});
+            this.cmbelegir.Location = new System.Drawing.Point(191, 15);
+            this.cmbelegir.Name = "cmbelegir";
+            this.cmbelegir.Size = new System.Drawing.Size(121, 21);
+            this.cmbelegir.TabIndex = 19;
+            this.cmbelegir.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(122, 15);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(63, 13);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "Ordenar por";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(890, 513);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.cmbelegir);
+            this.Controls.Add(this.btnactualizar);
+            this.Controls.Add(this.btneliminar);
+            this.Controls.Add(this.btnagregar);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.Txtdescripcion);
+            this.Controls.Add(this.txtcantidad);
+            this.Controls.Add(this.txtprecio);
+            this.Controls.Add(this.txtcaducidad);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmbmedida);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbldatos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.tbldatos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView tbldatos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.ComboBox cmbmedida;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtcaducidad;
+        private System.Windows.Forms.TextBox txtprecio;
+        private System.Windows.Forms.TextBox txtcantidad;
+        private System.Windows.Forms.TextBox Txtdescripcion;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button btnagregar;
+        private System.Windows.Forms.Button btneliminar;
+        private System.Windows.Forms.Button btnactualizar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Producto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Descripcion;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Precio;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Cantidad;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Caducidad;
+        private System.Windows.Forms.ComboBox cmbelegir;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Producto/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Producto
+{
+    public partial class Form1 : Form
+    {
+        int posicion;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void limpiar()
+        {
+            txtNombre.Clear();
+            txtprecio.Clear();
+            Txtdescripcion.Clear();
+            txtcaducidad.Clear();
+            txtcantidad.Clear();
+            txtId.Clear();
+            
+
+           
+        }
+        private void btnagregar_Click(object sender, EventArgs e)
+        {
+            String  descripcion, nombre, id,caducidad,unidadmedida,cantidad,precio;
+            int cantidadd, precioo;
+
+            nombre = txtNombre.Text;
+            if (nombre == "")
+            {
+                MessageBox.Show("Debe llenar la caja de texto con el nombre.",
+                                "Mensaje de Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (nombre is int)
+            {
+                MessageBox.Show($"Error, el nombre:{txtNombre.Text} no tiene el formato correcto.",
+                           "Mensaje de Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            else if (nombre is decimal)
+            {
+                MessageBox.Show($"Error, el nombre:{txtNombre.Text} no tiene el formato correcto.",
+                      "Mensaje de Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+
+
+
+
+
+            precio = txtprecio.Text;
+
+            descripcion = Txtdescripcion.Text;
+            if (descripcion == "")
+            {
+                MessageBox.Show("Debe llenar la caja de texto con el nombre.",
+                                "Mensaje de Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            id = txtId.Text;
+
+            if (id == "")
+            {
+                MessageBox.Show("Debe llenar la caja de texto con el nombre.",
+                                "Mensaje de Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+                caducidad = txtcaducidad.Text;
+            cantidad =txtcantidad.Text;
+            limpiar();
+           
+            tbldatos.Rows.Add(id,nombre, descripcion, precio,cantidad,caducidad);
+
+        }
+
+        private void tbldatos_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+           posicion = tbldatos.CurrentRow.Index;
+            txtId.Text = tbldatos[1, posicion].Value.ToString();
+            txtNombre.Text = tbldatos[2, posicion].Value.ToString();
+           Txtdescripcion.Text = tbldatos[3, posicion].Value.ToString();
+            txtprecio.Text = tbldatos[4, posicion].Value.ToString();
+            txtcantidad.Text= tbldatos[5, posicion].Value.ToString();
+            txtcaducidad.Text= tbldatos[6, posicion].Value.ToString();
+         
+        }
+
+        private void btnactualizar_Click(object sender, EventArgs e)
+        {
+            String precio, descripcion, producto, id, cantidad, caducidad, unidadmedida;
+            precio = txtprecio.Text;
+            descripcion = Txtdescripcion.Text;
+            producto = txtNombre.Text;
+            id = txtId.Text;
+            cantidad = txtcantidad.Text;
+            caducidad = txtcaducidad.Text;
+           
+            tbldatos[1, posicion].Value = txtId.Text;
+             tbldatos[2, posicion].Value =txtNombre.Text;
+             tbldatos[3, posicion].Value= Txtdescripcion.Text;
+           tbldatos[4, posicion].Value=txtprecio.Text;
+           tbldatos[5, posicion].Value = txtcantidad;
+           tbldatos[6, posicion].Value = txtcaducidad.Text;
+          
+        }
+
+        private void btneliminar_Click(object sender, EventArgs e)
+        {
+            tbldatos.Rows.RemoveAt(posicion);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
